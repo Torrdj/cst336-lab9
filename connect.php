464091ac49f92a94a2e8af36ff2cd68a490b1932
Leo TORDJMAN <ltordjman@csumb.edu>
2018-05-07 08:43:32 +0000

    done

---
+++ connect.php
@@ -0,0 +1,36 @@
+<?php
+
+function getDBConnection() {
+    
+    //C9 db info
+    $host = "us-cdbr-iron-east-05.cleardb.net";
+    $dbName = "heroku_96be87d910b5efe";
+    $username = "bf8752d16fa867";
+    $password = "dc52894c";
+    
+    //when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbName = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+    
+    try {
+        //Creates a database connection
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+    
+        // Setting Errorhandling to Exception
+        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    }
+    catch (PDOException $e) {
+       echo "Problems connecting to database!";
+       exit();
+    }
+    
+    
+    return $dbConn;
+}
+
+?>
